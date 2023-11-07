810440cb361e539ee3d365a287d2e2e56edab815
Knyc8 <kchen9712@bths.edu>
2023-11-06 20:50:23 -0500

    2

--- scripting.js
+++ scripting.js
@@ -14,20 +14,31 @@ document.getElementById('info').addEventListener('click', () => {
     }
 })
 
+// Testing
+function launched() {
+    console.log("Launched")
+}
+
 // Sounds
 function scare () {
     console.log("Sound");
-    var jumpscare = new Audio('res/pipes.mp3');
+    var jumpscare = new Audio('res/sfx/pipes.mp3');
     jumpscare.play();
 }
 
 function scare2 () {
     console.log("Sound");
-    var jumpscare = new Audio('res/fuzzy-jumpscare.mp3');
+    var jumpscare = new Audio('res/sfx/fuzzy-jumpscare.mp3');
     jumpscare.play();
 }
 
 function scare3 () {
     console.log("Sound");
-    var jumpscare = new Audio('res/jumpscare.mp3');
+    var jumpscare = new Audio('res/sfx/jumpscare.mp3');
+}
+
+function piano() {
+    console.log("Sound");
+    var pianoSong = new Audio('/res/sfx/BadEncounter.mp3');
+    pianoSong.play();
 }
